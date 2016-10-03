3a1efc6b039e3611f32409cdf8e9c50ddd340c18
C. Herrera <pro.cah@gmail.com>
2016-10-03 00:24:48 -0500

    Extra echo comments

--- cpu_to_max.sh
+++ cpu_to_max.sh
@@ -12,5 +12,6 @@
 #
 #
 echo "This program will try to eat resources "
+echo "Kill it when you feel the time is right."
 # This little program is a resources eater
 X=0;while [ 1 ];do echo $X;X=$((X+1));done
