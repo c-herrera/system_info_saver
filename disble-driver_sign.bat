721646c15b2796b6591c5a02b371283844185c3c
C. Herrera <pro.cah@gmail.com>
2016-10-13 21:17:59 -0500

    Checked the argument line
    Working now

--- disble-driver_sign.bat
+++ disble-driver_sign.bat
@@ -4,27 +4,44 @@
 title Disabling Driver Integrity Check
 
 @echo -----------------------------------------------------
-@echo Setting testsigning on
+@echo Trying to set testsigning %1
 @echo Remember to disable this when testing is done
-if %1 == "" goto noarguments
+if "%1" == "" goto badsyntax
 
 if "%1" == "on" (
-bcdedit /set testsigning on
+@echo -----------------------------------------------------
 @echo Setting DDISABLE_INTEGRITY_CHECKS on
-bcdedit.exe -set loadoptions DDISABLE_INTEGRITY_CHECKS
-bcdedit.exe -set TESTSIGNING ON
+bcdedit /set testsigning on
+@echo.
+bcdedit.exe /set loadoptions DDISABLE_INTEGRITY_CHECKS
+@echo.
+bcdedit.exe /set TESTSIGNING ON
+goto quit
 )
 
 if "%1" == "off" (
-bcdedit /set testsigning off
+@echo -----------------------------------------------------
 @echo Setting DDISABLE_INTEGRITY_CHECKS off
+bcdedit /set testsigning off
+@echo.
 bcdedit.exe -set TESTSIGNING OFF
+@echo.
+goto quit
 )
 
-:noarguments
-@echo To run this command properly  you must use the next sintax
-@echo disable_driver_sign on
+:badsyntax
+@cls
+@echo Incorrect sintax. To run this batch your input must be as follows :
+@echo.
+@echo.
+@echo.
+@echo. %0 on - off
+@echo. example :
+@echo. %0 on
+goto quit
 
 :quit
 @echo Done.
+
+
 
