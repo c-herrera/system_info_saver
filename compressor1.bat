dcb622687748df80a781f546110d083c7e63f985
C. Herrera <pro.cah@gmail.com>
2018-04-04 17:38:58 -0500

    Change conpressor batch
    Update on logic

--- compressor1.bat
+++ compressor1.bat
@@ -7,11 +7,11 @@
 SetLocal EnableDelayedExpansion EnableExtensions
 @echo off
 @cls
+
 set zipfolder=
 set zipapp=7z.exe
+set version=0.0.1
 set is_ok=
-
-@pause
 
 :: Look for 64 Program Files
 if exist %ProgramFiles% (
@@ -19,7 +19,7 @@ if exist %ProgramFiles% (
 		set zipfolder=%ProgramFiles%\7-Zip\7z.exe
 		@echo Folder is %zipfolder%
 		goto have7zip
-	)
+	) else goto failure
 )  
 
 if exist %ProgramFiles(x86)% (
@@ -27,24 +27,37 @@ if exist %ProgramFiles(x86)% (
 		set zipfolder=%ProgramFiles(x86)%\7-Zip\7z.exe
 		@echo Folder is %zipfolder%
 		goto have7zip
-	)
-)  
+	) else goto failure
+) 
 
 :have7zip
 @echo ------------------------------------------------
 @echo Starting batch compression at %date%-%time%
-@echo Path of 7 zip is %zipfolder%
 @echo Using 7 zip to compress the next folders in %CD%
 @echo ------------------------------------------------ 
 @dir /d
 @echo ------------------------------------------------ 
 for /d %%X in (*) do ( 
-@echo ============================================
-@echo Creating "%%X.zip" from "%%X\"
-@echo ============================================
-REM "c:\Program Files\7-Zip\7z.exe" a "%%X.zip" "%%X\"
-%zipfolder% a "%%X.zip" "%%X\"
+	@echo ============================================
+	@echo Creating "%%X.zip" from "%%X\"
+	@echo ============================================
+	REM "c:\Program Files\7-Zip\7z.exe" a "%%X.zip" "%%X\"
+	"%zipfolder%" a "%%X.zip" "%%X\"
+	if ERRORLEVEL == 1 goto failure
 ) 
+
 @echo Done!
+goto Quit
+
 :failure
+@echo xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 @echo An error has ocurred
+goto Quit
+
+:Quit
+@echo ============================================
+@echo Done with batch, no more operations from this point on.
+@echo Script : %0% 
+@echo version : %version%
+@echo ============================================
+exit /b
