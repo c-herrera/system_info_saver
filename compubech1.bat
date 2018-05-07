0da15c0989d03b31a6b1a2fe6ef75a35431d8621
C. Herrera <pro.cah@gmail.com>
2018-05-07 17:42:59 -0500

    Add files via upload

--- compubech1.bat
+++ compubech1.bat
@@ -7,8 +7,28 @@
 SetLocal EnableDelayedExpansion EnableExtensions
 @echo off
 @cls
-
+set testExeName=
 set version=0.0.1
+set input=0
+
+@echo -----------------------------------------------
+@echo Type the number of the focused test executable :
+@echo [1] Kishonti Compubench Mobile (CLI)
+@echo [2] Kishonti Compubench Desktop`
+@echo [9] Exit
+
+set /p input="Type the number of the test to run :"
+
+@echo --- %input%
+
+
+if /I "%input%"=="1" goto compubenchMobile
+if /I "%input%"=="2" goto compubenchDesktop
+if /I "%input%"=="9" goto quit
+
+
+:compubenchMobile
+set testExeName=CompubenchMobile_CLI_log.txt
 
 @echo off
 @echo Compubench-Mobile-CLI Test
@@ -16,55 +36,65 @@ set version=0.0.1
 @echo ============================================================
 @echo Face detect Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_facedetect_buff 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_facedetect_buff >> %testExeName%
 @pause
 @echo ============================================================
 @echo Face detect Test 2
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_facedetect_img 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_facedetect_img >> %testExeName%
 @pause
 @echo ============================================================
 @echo Fluid 32k Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_fluid_32k 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_fluid_32k >> %testExeName%
 @pause
 @echo ============================================================
 @echo Fractal Julia Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_fractal_juliaset 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_fractal_juliaset >> %testExeName%
 @pause
 @echo ============================================================
 @echo Gauss Buffered Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_gauss_buff 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_gauss_buff >> %testExeName%
 @pause
 @echo ============================================================
 @echo Gauss Image Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_gauss_img 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_gauss_img >> %testExeName%
 @pause
 @echo ============================================================
 @echo Histogram Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_histogram_buff 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_histogram_buff >> %testExeName%
 @pause
 @echo ============================================================
 @echo Face detect Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_histogram_img 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_histogram_img >> %testExeName%
 @pause
 @echo ============================================================
 @echo Face detect Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_raytrace_ambocc 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_raytrace_ambocc >> %testExeName%
 @pause
 @echo ============================================================
 @echo Face detect Test
 @timeout 2 > nul
-Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_raytrace_raycast 
+Compubench-Mobile-CLI.exe --gfx=glfw --test_id=cl_raytrace_raycast >> %testExeName%
 @pause
+goto quit
 
-:Quit
+:compubenchDesktop
+set testExeName=CompubenchDesktop_CLI_log.txt
+
+@echo off
+@echo Compubench-Desktop-CLI Test
+@echo Test Start
+@echo ============================================================
+goto quit
+
+:quit
 @echo ============================================
 @echo Done with batch, no more operations from this point on.
 @echo Script : %0% 
